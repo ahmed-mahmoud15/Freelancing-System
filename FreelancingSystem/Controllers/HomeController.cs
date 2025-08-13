e27db16d5fd9ba4dbdc9ef5f0bfafac41332af0e
hamata <ahmed.adham.1543@gmail.com>
2025-08-13 16:31:19 +0300

    .

--- FreelancingSystem/Controllers/HomeController.cs
+++ FreelancingSystem/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace FreelancingSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
